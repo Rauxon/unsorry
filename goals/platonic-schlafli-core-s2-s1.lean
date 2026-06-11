788764cf1b06b47abda7298c336c43304e29ffe4
Chris Barlow <cgbarlow@gmail.com>
2026-06-11 05:48:01 +0000

    decompose(platonic-schlafli-core-s2): 3 sub-lemmas by p3-alpha

---
+++ goals/platonic-schlafli-core-s2-s1.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem nat_inv_le_third_of_three_le (q : ℕ) (hq : 3 ≤ q) : (q : ℚ)⁻¹ ≤ (3 : ℚ)⁻¹ := by
+  sorry
---
+++ goals/platonic-schlafli-core-s2-s2.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem rat_gt_sixth_of_add_gt_half (a b : ℚ) (hab : (2 : ℚ)⁻¹ < a + b) (hb : b ≤ (3 : ℚ)⁻¹) : (6 : ℚ)⁻¹ < a := by
+  sorry
