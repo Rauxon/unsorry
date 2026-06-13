b5c8cbad6b2f8e725449ac0b2e31cfbfadb7bbdc
Chris Barlow <cgbarlow@gmail.com>
2026-06-14 00:47:04 +1200

    decompose(fourth-power-mod-five): 2 sub-lemmas by p3-b1 (#317)

---
+++ goals/fourth-power-mod-five-s1.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem fourth_power_mod_five_reduce (n : ℕ) : n ^ 4 % 5 = (n % 5) ^ 4 % 5 := by
+  sorry
---
+++ goals/fourth-power-mod-five-s2.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem fourth_power_residue_mod_five (r : ℕ) (hr0 : 1 ≤ r) (hr : r < 5) : r ^ 4 % 5 = 1 := by
+  sorry
