6924783e4f31d96c1ba50cf891512f1548dbc9b7
Perttu Isotalo <201641+perttu@users.noreply.github.com>
2026-06-14 12:22:30 +0300

    decompose(nat-sq-lt-two-pow-s2): 3 sub-lemmas by oma-2-c50d

---
+++ goals/nat-sq-lt-two-pow-s2-s1.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem sq_succ_le_two_mul_sq_of_three_le {n : ℕ} (hn : 3 ≤ n) : (n + 1) ^ 2 ≤ 2 * n ^ 2 := by
+  sorry
---
+++ goals/nat-sq-lt-two-pow-s2-s2.lean
@@ -0,0 +1,4 @@
+import Mathlib
+
+theorem two_mul_sq_lt_two_mul_pow_of_sq_lt_pow {n : ℕ} (h : n ^ 2 < 2 ^ n) : 2 * n ^ 2 < 2 * 2 ^ n := by
+  sorry
