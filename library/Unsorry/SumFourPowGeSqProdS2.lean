3304dc1ff879a69e293cd3ead47812ff14df9ed1
Adam Holt <adam@adamholt.co.nz>
2026-06-14 19:46:29 +0000

    prove(sum-four-pow-ge-sq-prod-s2): cyclic fourth powers identity

---
+++ library/Unsorry/SumFourPowGeSqProdS2.lean
@@ -0,0 +1,6 @@
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic.Ring
+
+theorem cyclic_half_sum_fourth_eq_sum_fourth (a b c : ℝ) :
+    (a^4 + b^4) / 2 + (b^4 + c^4) / 2 + (c^4 + a^4) / 2 = a^4 + b^4 + c^4 := by
+  ring_nf
