67b6865e8b49ca73f9401a451bd49209cf2bb76a
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 18:47:00 -0400

    prove(gcd-twon-n5-dvd-ten): gcd_twon_n5_dvd_ten by mac-158f

--- library/Unsorry/GcdTwonN5DvdTen.lean
+++ library/Unsorry/GcdTwonN5DvdTen.lean
@@ -1,9 +1,13 @@
 import Mathlib
 
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
 theorem gcd_twon_n5_dvd_ten (n : ℕ) : Nat.gcd (2 * n) (n + 5) ∣ 10 := by
-  have h1 : Nat.gcd (2 * n) (n + 5) ∣ (2 * n) := Nat.gcd_dvd_left _ _
-  have h2 : Nat.gcd (2 * n) (n + 5) ∣ (n + 5) := Nat.gcd_dvd_right _ _
-  have h3 : Nat.gcd (2 * n) (n + 5) ∣ 2 * n + 10 := by
-    have heq : 2 * n + 10 = 2 * (n + 5) := by ring
-    rw [heq]; exact h2.mul_left 2
-  exact (Nat.dvd_add_right h1).mp h3
+  have h1 := Nat.gcd_dvd_left (2 * n) (n + 5)
+  have h2 := Nat.gcd_dvd_right (2 * n) (n + 5)
+  have hA : Nat.gcd (2 * n) (n + 5) ∣ 2 * (n + 5) := h2.mul_left 2
+  have hB : Nat.gcd (2 * n) (n + 5) ∣ 1 * (2 * n) := h1.mul_left 1
+  have key : 2 * (n + 5) = 1 * (2 * n) + 10 := by omega
+  rw [key] at hA
+  exact ((Nat.dvd_add_right hB).mp hA).trans (by norm_num)
