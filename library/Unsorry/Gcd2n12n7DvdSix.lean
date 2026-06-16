149b3a932d5be8f927b307e4d8bf450cbbba99bd
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 18:45:56 -0400

    prove(gcd-2n1-2n7-dvd-six): gcd_2n1_2n7_dvd_six by mac-158f

--- library/Unsorry/Gcd2n12n7DvdSix.lean
+++ library/Unsorry/Gcd2n12n7DvdSix.lean
@@ -1,9 +1,13 @@
 import Mathlib
 
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
 theorem gcd_2n1_2n7_dvd_six (n : ℕ) : Nat.gcd (2 * n + 1) (2 * n + 7) ∣ 6 := by
-  have h1 : Nat.gcd (2 * n + 1) (2 * n + 7) ∣ (2 * n + 1) := Nat.gcd_dvd_left _ _
-  have h2 : Nat.gcd (2 * n + 1) (2 * n + 7) ∣ (2 * n + 1) + 6 := by
-    have heq : 2 * n + 7 = (2 * n + 1) + 6 := by omega
-    rw [← heq]
-    exact Nat.gcd_dvd_right _ _
-  exact (Nat.dvd_add_right h1).mp h2
+  have h1 := Nat.gcd_dvd_left (2 * n + 1) (2 * n + 7)
+  have h2 := Nat.gcd_dvd_right (2 * n + 1) (2 * n + 7)
+  have hA : Nat.gcd (2 * n + 1) (2 * n + 7) ∣ 1 * (2 * n + 7) := h2.mul_left 1
+  have hB : Nat.gcd (2 * n + 1) (2 * n + 7) ∣ 1 * (2 * n + 1) := h1.mul_left 1
+  have key : 1 * (2 * n + 7) = 1 * (2 * n + 1) + 6 := by omega
+  rw [key] at hA
+  exact ((Nat.dvd_add_right hB).mp hA).trans (by norm_num)
