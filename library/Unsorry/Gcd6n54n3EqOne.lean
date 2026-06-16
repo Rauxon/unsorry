56ece7eb8431c46fea4503aaf5dad10b8ae6bc5d
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 19:01:56 -0400

    prove(gcd-6n5-4n3-eq-one): gcd_6n5_4n3_eq_one by mac-158f

--- library/Unsorry/Gcd6n54n3EqOne.lean
+++ library/Unsorry/Gcd6n54n3EqOne.lean
@@ -1,11 +1,13 @@
 import Mathlib
 
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
 theorem gcd_6n5_4n3_eq_one (n : ℕ) : Nat.gcd (6 * n + 5) (4 * n + 3) = 1 := by
-  have h1 : Nat.gcd (6 * n + 5) (4 * n + 3) ∣ (6 * n + 5) := Nat.gcd_dvd_left _ _
-  have h2 : Nat.gcd (6 * n + 5) (4 * n + 3) ∣ (4 * n + 3) := Nat.gcd_dvd_right _ _
-  have h3 : Nat.gcd (6 * n + 5) (4 * n + 3) ∣ 3 * (4 * n + 3) := h2.mul_left 3
-  have h4 : Nat.gcd (6 * n + 5) (4 * n + 3) ∣ 3 * (4 * n + 3) + 1 := by
-    have heq : 3 * (4 * n + 3) + 1 = 2 * (6 * n + 5) := by omega
-    rw [heq]
-    exact h1.mul_left 2
-  exact Nat.dvd_one.mp ((Nat.dvd_add_right h3).mp h4)
+  have h1 := Nat.gcd_dvd_left (6 * n + 5) (4 * n + 3)
+  have h2 := Nat.gcd_dvd_right (6 * n + 5) (4 * n + 3)
+  have hA : Nat.gcd (6 * n + 5) (4 * n + 3) ∣ 2 * (6 * n + 5) := h1.mul_left 2
+  have hB : Nat.gcd (6 * n + 5) (4 * n + 3) ∣ 3 * (4 * n + 3) := h2.mul_left 3
+  have key : 2 * (6 * n + 5) = 3 * (4 * n + 3) + 1 := by omega
+  rw [key] at hA
+  exact Nat.dvd_one.mp ((Nat.dvd_add_right hB).mp hA)
