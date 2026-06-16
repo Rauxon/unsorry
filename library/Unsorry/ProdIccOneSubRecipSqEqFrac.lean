0ebbd27d6b9ccfa6147942420dcce1f5d6c31b9c
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 18:25:28 -0400

    prove(prod-icc-one-sub-recip-sq-eq-frac): prod_icc_one_sub_recip_sq_eq_frac by mac-158f

---
+++ library/Unsorry/ProdIccOneSubRecipSqEqFrac.lean
@@ -0,0 +1,25 @@
+import Mathlib
+
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
+theorem prod_icc_one_sub_recip_sq_eq_frac (n : ℕ) (hn : 2 ≤ n) : ∏ k ∈ Finset.Icc 2 n, (((k : ℚ) ^ 2 - 1) / (k : ℚ) ^ 2) = ((n : ℚ) + 1) / (2 * (n : ℚ)) := by
+  induction n, hn using Nat.le_induction with
+  | base => first | (norm_num [Finset.Icc_self]) | (simp only [Finset.Icc_self, Finset.sum_singleton, Finset.prod_singleton]; norm_num) | (simp [Finset.Icc_self]; norm_num) | (simp [Finset.Icc_self]; ring) | (simp [Finset.Icc_self]; push_cast; field_simp; ring) | (simp [Finset.Icc_self]; field_simp; ring) | rfl | decide
+  | succ n hn ih =>
+    rw [Finset.prod_Icc_succ_top (by omega)]
+    try have hzb : ((n:ℚ)) ≠ 0 := by exact_mod_cast (by omega : n ≠ 0)
+    try have hzc : ((n:ℚ) + 1) ≠ 0 := by positivity
+    try have hzd : ((n:ℚ) + 2) ≠ 0 := by positivity
+    try have hze : ((n:ℚ) + 3) ≠ 0 := by positivity
+    try have hzf : ((n:ℚ) - 1) ≠ 0 := sub_ne_zero.mpr (by exact_mod_cast (by omega : n ≠ 1))
+    first
+      | (rw [ih]; push_cast; field_simp; ring)
+      | (rw [ih]; field_simp; ring)
+      | (rw [ih]; push_cast; field_simp; ring_nf; done)
+      | (rw [ih]; field_simp; ring_nf; done)
+      | (rw [ih]; push_cast; ring)
+      | (rw [ih]; ring)
+      | (rw [ih]; push_cast [Nat.factorial_succ]; field_simp; ring)
+      | (simp only [Nat.factorial_succ]; rw [ih]; push_cast; field_simp; ring)
+      | (simp only [ih]; push_cast; field_simp; ring)
