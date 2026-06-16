364c868533f08ad52057ec66026607259dbbe2eb
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 19:01:46 -0400

    prove(gcd-5n2-7n3-eq-one): gcd_5n2_7n3_eq_one by mac-158f

--- library/Unsorry/Gcd5n27n3EqOne.lean
+++ library/Unsorry/Gcd5n27n3EqOne.lean
@@ -1,11 +1,13 @@
 import Mathlib
 
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
 theorem gcd_5n2_7n3_eq_one (n : ℕ) : Nat.gcd (5 * n + 2) (7 * n + 3) = 1 := by
-  have h1 : Nat.gcd (5 * n + 2) (7 * n + 3) ∣ (5 * n + 2) := Nat.gcd_dvd_left _ _
-  have h2 : Nat.gcd (5 * n + 2) (7 * n + 3) ∣ (7 * n + 3) := Nat.gcd_dvd_right _ _
-  have h7 : Nat.gcd (5 * n + 2) (7 * n + 3) ∣ 7 * (5 * n + 2) := h1.mul_left 7
-  have h5 : Nat.gcd (5 * n + 2) (7 * n + 3) ∣ 7 * (5 * n + 2) + 1 := by
-    have heq : 7 * (5 * n + 2) + 1 = 5 * (7 * n + 3) := by omega
-    rw [heq]
-    exact h2.mul_left 5
-  exact Nat.dvd_one.mp ((Nat.dvd_add_right h7).mp h5)
+  have h1 := Nat.gcd_dvd_left (5 * n + 2) (7 * n + 3)
+  have h2 := Nat.gcd_dvd_right (5 * n + 2) (7 * n + 3)
+  have hA : Nat.gcd (5 * n + 2) (7 * n + 3) ∣ 5 * (7 * n + 3) := h2.mul_left 5
+  have hB : Nat.gcd (5 * n + 2) (7 * n + 3) ∣ 7 * (5 * n + 2) := h1.mul_left 7
+  have key : 5 * (7 * n + 3) = 7 * (5 * n + 2) + 1 := by omega
+  rw [key] at hA
+  exact Nat.dvd_one.mp ((Nat.dvd_add_right hB).mp hA)
