e23a8a59773a7408ea1140355912cffc67313c11
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 18:46:54 -0400

    prove(gcd-threen-n7-dvd-twentyone): gcd_threen_n7_dvd_twentyone by mac-158f

--- library/Unsorry/GcdThreenN7DvdTwentyone.lean
+++ library/Unsorry/GcdThreenN7DvdTwentyone.lean
@@ -1,9 +1,13 @@
 import Mathlib
 
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
 theorem gcd_threen_n7_dvd_twentyone (n : ℕ) : Nat.gcd (3 * n) (n + 7) ∣ 21 := by
-  have h1 : Nat.gcd (3 * n) (n + 7) ∣ (3 * n) := Nat.gcd_dvd_left _ _
-  have h2 : Nat.gcd (3 * n) (n + 7) ∣ (n + 7) := Nat.gcd_dvd_right _ _
-  have h3 : Nat.gcd (3 * n) (n + 7) ∣ 3 * n + 21 := by
-    have heq : 3 * n + 21 = 3 * (n + 7) := by ring
-    rw [heq]; exact h2.mul_left 3
-  exact (Nat.dvd_add_right h1).mp h3
+  have h1 := Nat.gcd_dvd_left (3 * n) (n + 7)
+  have h2 := Nat.gcd_dvd_right (3 * n) (n + 7)
+  have hA : Nat.gcd (3 * n) (n + 7) ∣ 3 * (n + 7) := h2.mul_left 3
+  have hB : Nat.gcd (3 * n) (n + 7) ∣ 1 * (3 * n) := h1.mul_left 1
+  have key : 3 * (n + 7) = 1 * (3 * n) + 21 := by omega
+  rw [key] at hA
+  exact ((Nat.dvd_add_right hB).mp hA).trans (by norm_num)
