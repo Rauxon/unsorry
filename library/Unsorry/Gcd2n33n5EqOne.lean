df93a77f5454292dd6802a171bb71995ceb4e94d
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 19:01:12 -0400

    prove(gcd-2n3-3n5-eq-one): gcd_2n3_3n5_eq_one by mac-158f

--- library/Unsorry/Gcd2n33n5EqOne.lean
+++ library/Unsorry/Gcd2n33n5EqOne.lean
@@ -1,10 +1,13 @@
 import Mathlib
 
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
 theorem gcd_2n3_3n5_eq_one (n : ℕ) : Nat.gcd (2 * n + 3) (3 * n + 5) = 1 := by
-  have h1 : Nat.gcd (2 * n + 3) (3 * n + 5) ∣ (2 * n + 3) := Nat.gcd_dvd_left _ _
-  have h2 : Nat.gcd (2 * n + 3) (3 * n + 5) ∣ (3 * n + 5) := Nat.gcd_dvd_right _ _
-  have h3 : Nat.gcd (2 * n + 3) (3 * n + 5) ∣ 3 * (2 * n + 3) := h1.mul_left 3
-  have h4 : Nat.gcd (2 * n + 3) (3 * n + 5) ∣ 3 * (2 * n + 3) + 1 := by
-    have heq : 3 * (2 * n + 3) + 1 = 2 * (3 * n + 5) := by omega
-    rw [heq]; exact h2.mul_left 2
-  exact Nat.dvd_one.mp ((Nat.dvd_add_right h3).mp h4)
+  have h1 := Nat.gcd_dvd_left (2 * n + 3) (3 * n + 5)
+  have h2 := Nat.gcd_dvd_right (2 * n + 3) (3 * n + 5)
+  have hA : Nat.gcd (2 * n + 3) (3 * n + 5) ∣ 2 * (3 * n + 5) := h2.mul_left 2
+  have hB : Nat.gcd (2 * n + 3) (3 * n + 5) ∣ 3 * (2 * n + 3) := h1.mul_left 3
+  have key : 2 * (3 * n + 5) = 3 * (2 * n + 3) + 1 := by omega
+  rw [key] at hA
+  exact Nat.dvd_one.mp ((Nat.dvd_add_right hB).mp hA)
