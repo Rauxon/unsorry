38a418d899a909d0e824e8212b422e0adfbeef62
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 19:02:07 -0400

    prove(gcd-n3-2n7-eq-one): gcd_n3_2n7_eq_one by mac-158f

--- library/Unsorry/GcdN32n7EqOne.lean
+++ library/Unsorry/GcdN32n7EqOne.lean
@@ -1,11 +1,13 @@
 import Mathlib
 
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
 theorem gcd_n3_2n7_eq_one (n : ℕ) : Nat.gcd (n + 3) (2 * n + 7) = 1 := by
-  have h1 : Nat.gcd (n + 3) (2 * n + 7) ∣ (n + 3) := Nat.gcd_dvd_left _ _
-  have h2 : Nat.gcd (n + 3) (2 * n + 7) ∣ (2 * n + 7) := Nat.gcd_dvd_right _ _
-  have h3 : Nat.gcd (n + 3) (2 * n + 7) ∣ 2 * (n + 3) := h1.mul_left 2
-  have h4 : Nat.gcd (n + 3) (2 * n + 7) ∣ 2 * (n + 3) + 1 := by
-    have heq : 2 * (n + 3) + 1 = 2 * n + 7 := by omega
-    rw [heq]
-    exact h2
-  exact Nat.dvd_one.mp ((Nat.dvd_add_right h3).mp h4)
+  have h1 := Nat.gcd_dvd_left (n + 3) (2 * n + 7)
+  have h2 := Nat.gcd_dvd_right (n + 3) (2 * n + 7)
+  have hA : Nat.gcd (n + 3) (2 * n + 7) ∣ 1 * (2 * n + 7) := h2.mul_left 1
+  have hB : Nat.gcd (n + 3) (2 * n + 7) ∣ 2 * (n + 3) := h1.mul_left 2
+  have key : 1 * (2 * n + 7) = 2 * (n + 3) + 1 := by omega
+  rw [key] at hA
+  exact Nat.dvd_one.mp ((Nat.dvd_add_right hB).mp hA)
