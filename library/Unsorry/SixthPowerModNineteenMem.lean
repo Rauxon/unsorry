bd76e6ef6e65744779d0d3ec2a5663c55789751a
OceanLi <quantocean.li@gmail.com>
2026-06-16 15:24:50 -0400

    prove(sixth-power-mod-nineteen-mem): sixth_power_mod_nineteen_mem by mac-158f (#1179)

---
+++ library/Unsorry/SixthPowerModNineteenMem.lean
@@ -0,0 +1,6 @@
+import Mathlib
+
+theorem sixth_power_mod_nineteen_mem (n : ℕ) : n ^ 6 % 19 = 0 ∨ n ^ 6 % 19 = 1 ∨ n ^ 6 % 19 = 7 ∨ n ^ 6 % 19 = 11 := by
+  rw [Nat.pow_mod]
+  have h : n % 19 < 19 := Nat.mod_lt n (by norm_num)
+  interval_cases (n % 19) <;> decide
