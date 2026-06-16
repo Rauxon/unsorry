1e06eed26c08af6184181e93cf7da1b06ed82ef5
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 18:45:51 -0400

    prove(gcd-2n1-2n5-dvd-four): gcd_2n1_2n5_dvd_four by mac-158f

--- library/Unsorry/Gcd2n12n5DvdFour.lean
+++ library/Unsorry/Gcd2n12n5DvdFour.lean
@@ -1,9 +1,13 @@
 import Mathlib
 
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
 theorem gcd_2n1_2n5_dvd_four (n : ℕ) : Nat.gcd (2 * n + 1) (2 * n + 5) ∣ 4 := by
-  have h1 : Nat.gcd (2 * n + 1) (2 * n + 5) ∣ (2 * n + 1) := Nat.gcd_dvd_left _ _
-  have h2 : Nat.gcd (2 * n + 1) (2 * n + 5) ∣ (2 * n + 1) + 4 := by
-    have heq : 2 * n + 5 = (2 * n + 1) + 4 := by omega
-    rw [← heq]
-    exact Nat.gcd_dvd_right _ _
-  exact (Nat.dvd_add_right h1).mp h2
+  have h1 := Nat.gcd_dvd_left (2 * n + 1) (2 * n + 5)
+  have h2 := Nat.gcd_dvd_right (2 * n + 1) (2 * n + 5)
+  have hA : Nat.gcd (2 * n + 1) (2 * n + 5) ∣ 1 * (2 * n + 5) := h2.mul_left 1
+  have hB : Nat.gcd (2 * n + 1) (2 * n + 5) ∣ 1 * (2 * n + 1) := h1.mul_left 1
+  have key : 1 * (2 * n + 5) = 1 * (2 * n + 1) + 4 := by omega
+  rw [key] at hA
+  exact ((Nat.dvd_add_right hB).mp hA).trans (by norm_num)
