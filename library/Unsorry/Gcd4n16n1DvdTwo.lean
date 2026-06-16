5055c2a02f20bfdbfb478ac8c7f5d6178204d56d
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 18:46:14 -0400

    prove(gcd-4n1-6n1-dvd-two): gcd_4n1_6n1_dvd_two by mac-158f

--- library/Unsorry/Gcd4n16n1DvdTwo.lean
+++ library/Unsorry/Gcd4n16n1DvdTwo.lean
@@ -1,12 +1,13 @@
 import Mathlib
 
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
 theorem gcd_4n1_6n1_dvd_two (n : ℕ) : Nat.gcd (4 * n + 1) (6 * n + 1) ∣ 2 := by
-  have h1 : Nat.gcd (4 * n + 1) (6 * n + 1) ∣ (4 * n + 1) := Nat.gcd_dvd_left _ _
-  have h2 : Nat.gcd (4 * n + 1) (6 * n + 1) ∣ (6 * n + 1) := Nat.gcd_dvd_right _ _
-  have h3 : Nat.gcd (4 * n + 1) (6 * n + 1) ∣ 2 * (6 * n + 1) := h2.mul_left 2
-  have h4 : Nat.gcd (4 * n + 1) (6 * n + 1) ∣ 2 * (6 * n + 1) + 1 := by
-    have heq : 2 * (6 * n + 1) + 1 = 3 * (4 * n + 1) := by omega
-    rw [heq]
-    exact h1.mul_left 3
-  have hone : Nat.gcd (4 * n + 1) (6 * n + 1) ∣ 1 := (Nat.dvd_add_right h3).mp h4
-  exact hone.trans (one_dvd 2)
+  have h1 := Nat.gcd_dvd_left (4 * n + 1) (6 * n + 1)
+  have h2 := Nat.gcd_dvd_right (4 * n + 1) (6 * n + 1)
+  have hA : Nat.gcd (4 * n + 1) (6 * n + 1) ∣ 3 * (4 * n + 1) := h1.mul_left 3
+  have hB : Nat.gcd (4 * n + 1) (6 * n + 1) ∣ 2 * (6 * n + 1) := h2.mul_left 2
+  have key : 3 * (4 * n + 1) = 2 * (6 * n + 1) + 1 := by omega
+  rw [key] at hA
+  exact ((Nat.dvd_add_right hB).mp hA).trans (by norm_num)
