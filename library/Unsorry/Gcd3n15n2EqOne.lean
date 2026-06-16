6c3c3e6485af14a7ace231eab78943a11cf13abc
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 19:01:23 -0400

    prove(gcd-3n1-5n2-eq-one): gcd_3n1_5n2_eq_one by mac-158f

--- library/Unsorry/Gcd3n15n2EqOne.lean
+++ library/Unsorry/Gcd3n15n2EqOne.lean
@@ -1,11 +1,13 @@
 import Mathlib
 
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
 theorem gcd_3n1_5n2_eq_one (n : ℕ) : Nat.gcd (3 * n + 1) (5 * n + 2) = 1 := by
-  have h1 : Nat.gcd (3 * n + 1) (5 * n + 2) ∣ (3 * n + 1) := Nat.gcd_dvd_left _ _
-  have h2 : Nat.gcd (3 * n + 1) (5 * n + 2) ∣ (5 * n + 2) := Nat.gcd_dvd_right _ _
-  have h3 : Nat.gcd (3 * n + 1) (5 * n + 2) ∣ 5 * (3 * n + 1) := h1.mul_left 5
-  have h4 : Nat.gcd (3 * n + 1) (5 * n + 2) ∣ 5 * (3 * n + 1) + 1 := by
-    have heq : 5 * (3 * n + 1) + 1 = 3 * (5 * n + 2) := by omega
-    rw [heq]
-    exact h2.mul_left 3
-  exact Nat.dvd_one.mp ((Nat.dvd_add_right h3).mp h4)
+  have h1 := Nat.gcd_dvd_left (3 * n + 1) (5 * n + 2)
+  have h2 := Nat.gcd_dvd_right (3 * n + 1) (5 * n + 2)
+  have hA : Nat.gcd (3 * n + 1) (5 * n + 2) ∣ 3 * (5 * n + 2) := h2.mul_left 3
+  have hB : Nat.gcd (3 * n + 1) (5 * n + 2) ∣ 5 * (3 * n + 1) := h1.mul_left 5
+  have key : 3 * (5 * n + 2) = 5 * (3 * n + 1) + 1 := by omega
+  rw [key] at hA
+  exact Nat.dvd_one.mp ((Nat.dvd_add_right hB).mp hA)
