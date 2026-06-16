56ee9a332aff44e187b9e6e672d8404f16ef8619
ruv <ruv@ruv.net>
2026-06-16 17:43:32 -0400

    prove(lucas-succ-add-lucas-pred-eq-five-mul-fib): lucas_succ_add_lucas_pred_eq_five_mul_fib (solver: ruvnet, non-trivial)

---
+++ library/Unsorry/LucasSuccAddLucasPredEqFiveMulFib.lean
@@ -0,0 +1,6 @@
+import Mathlib
+
+theorem lucas_succ_add_lucas_pred_eq_five_mul_fib (n : ℕ) :
+    (Nat.fib (n + 2) + Nat.fib (n + 4)) + (Nat.fib n + Nat.fib (n + 2)) = 5 * Nat.fib (n + 2) := by
+  simp only [Nat.fib_add_two]
+  ring
