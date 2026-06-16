2480d5db4d2803296a888bf95f1702692058dead
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 15:13:58 -0400

    prove(sq-mod-forty-mem): sq_mod_forty_mem by mac-158f

---
+++ library/Unsorry/SqModFortyMem.lean
@@ -0,0 +1,6 @@
+import Mathlib
+
+theorem sq_mod_forty_mem (n : ℕ) : n ^ 2 % 40 = 0 ∨ n ^ 2 % 40 = 1 ∨ n ^ 2 % 40 = 4 ∨ n ^ 2 % 40 = 9 ∨ n ^ 2 % 40 = 16 ∨ n ^ 2 % 40 = 20 ∨ n ^ 2 % 40 = 24 ∨ n ^ 2 % 40 = 25 ∨ n ^ 2 % 40 = 36 := by
+  rw [Nat.pow_mod]
+  have h : n % 40 < 40 := Nat.mod_lt n (by norm_num)
+  interval_cases (n % 40) <;> decide
