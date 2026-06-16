3e335ba30e87b8b849370d17a8140c1831d0ad7e
ruv <ruv@ruv.net>
2026-06-16 17:46:28 -0400

    chore: remove stray temp files accidentally staged (concurrent-workflow artifact)

--- w5_dvd-fortyeight-coprime-six-pow-four-sub-one.lean
+++
@@ -1,6 +0,0 @@
-import Mathlib
-
-set_option maxRecDepth 8000 in
-theorem dvd_fortyeight_coprime_six_pow_four_sub_one (n : ℤ) (h2 : ¬ (2 : ℤ) ∣ n) (h3 : ¬ (3 : ℤ) ∣ n) : (48 : ℤ) ∣ n ^ 4 - 1 := by
-  have e2 : n % 2 = 1 := by omega_nat <;> omega
-  sorry
