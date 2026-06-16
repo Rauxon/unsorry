454051430cae3f3b6942ce3b341f75cde72a9da0
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2026-06-16 18:46:19 -0400

    prove(gcd-n-add-six-dvd-six): gcd_n_add_six_dvd_six by mac-158f

--- library/Unsorry/GcdNAddSixDvdSix.lean
+++ library/Unsorry/GcdNAddSixDvdSix.lean
@@ -1,4 +1,13 @@
 import Mathlib
 
+set_option linter.unusedTactic false in
+set_option linter.unreachableTactic false in
+set_option linter.unusedVariables false in
 theorem gcd_n_add_six_dvd_six (n : ℕ) : Nat.gcd n (n + 6) ∣ 6 := by
-  exact (Nat.dvd_add_right (Nat.gcd_dvd_left n (n + 6))).mp (Nat.gcd_dvd_right n (n + 6))
+  have h1 := Nat.gcd_dvd_left (n) (n + 6)
+  have h2 := Nat.gcd_dvd_right (n) (n + 6)
+  have hA : Nat.gcd (n) (n + 6) ∣ 1 * (n + 6) := h2.mul_left 1
+  have hB : Nat.gcd (n) (n + 6) ∣ 1 * (n) := h1.mul_left 1
+  have key : 1 * (n + 6) = 1 * (n) + 6 := by omega
+  rw [key] at hA
+  exact ((Nat.dvd_add_right hB).mp hA).trans (by norm_num)
